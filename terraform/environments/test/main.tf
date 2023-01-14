b0cacd812a01dde377df1ca53aa1cf6cbcf10b3b
bhuwanonline <bhuwanonline@gmail.com>
2023-01-14 11:36:35 +0530

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -13,11 +13,7 @@ terraform {
     access_key           = "zzuNJIqW7AuTo30SZFsn5JxucVP9+VzFOpoPAk3DmLGokddGl/GdGLopaxcRcYwgyVP8nDu8NBAD+ASt0MrzAw=="
   }
 }
-module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
-  location             = "${var.location}"
-}
+
 module "network" {
   source               = "../../modules/network"
   address_space        = "${var.address_space}"
@@ -25,7 +21,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
+  resource_group       = "${var.resource_group_name}"
   address_prefixes     = "${var.address_prefixes}"
 }
 
@@ -34,7 +30,7 @@ module "nsg-test" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -43,20 +39,20 @@ module "appservice" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
 }
 
 module "vm" {
   source               = "../../modules/vm"
   location             = "${var.location}"
-  resource_group       = "${module.resource_group.resource_group_name}"
+  resource_group       = "${var.resource_group_name}"
   subnet_id            = "${module.network.subnet_id_test}"
   public_ip_address_id = "${module.publicip.public_ip_address_id}" 
   admin_username       = "${var.admin_username}"
